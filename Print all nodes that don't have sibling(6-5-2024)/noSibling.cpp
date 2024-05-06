d8c6ff66d8cd1691a6a4fbf5f2acab7e72877541
Anurag Pandey <anuragpandey88699@gmail.com>
2024-05-06 23:44:23 +0530

    new

---
+++ Print all nodes that don't have sibling(6-5-2024)/noSibling.cpp
@@ -0,0 +1,31 @@
+struct Node
+ {
+     int data;
+     Node* left, *right;
+}; */
+
+
+ void solve(Node *node,vector<int>&v )
+ {
+     if(node==nullptr)
+     return;
+     if((node->left==nullptr)^(node->right==nullptr))
+     {
+         if(node->left!=nullptr)
+         v.push_back(node->left->data);
+         else
+          v.push_back(node->right->data);
+         
+     }
+     solve(node->left,v);
+     solve(node->right,v);
+     
+ }
+vector<int> noSibling(Node* node)
+{
+    // code here
+    vector<int>ans;
+    solve(node,ans);
+    sort(ans.begin(),ans.end());
+    return ans.size()==0?vector<int>{-1}:ans;
+}
